d687cf540c79cafb637f3275b748358da03e2c95
cury009 <63730861+cury009@users.noreply.github.com>
2020-12-18 17:57:03 +0100

    con estilo propio

--- app/src/main/java/com/example/formulario2amarillo/MainActivity.java
+++ app/src/main/java/com/example/formulario2amarillo/MainActivity.java
@@ -7,22 +7,26 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
 
     Spinner sp_combo1 = null;
+    TextView txt_item_seleccionado = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sp_combo1 = (Spinner) findViewById(R.id.sp_combo1);
+        txt_item_seleccionado = (TextView) findViewById(R.id.txt_item_seleccionado);
 
         //----------------------------------------------------
+        sp_combo1.setOnItemSelectedListener(this);
         //cargar el combo con los datos
 
-        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this, R.array.frutas, android.R.layout.simple_spinner_item);
-        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this, R.array.frutas, R.layout.casilladesplegable);
+        adaptador.setDropDownViewResource(R.layout.casilladesplegable);
         sp_combo1.setAdapter(adaptador);
 
     }
@@ -30,7 +34,8 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String valorItem = String.valueOf(sp_combo1.getItemAtPosition(position));
-        //Toast.makeText(parent.getContext(), this, "has elegido "+valorItem, Toast.LENGTH_SHORT).show();
+        txt_item_seleccionado.setText(valorItem);
+        Toast.makeText( parent.getContext(), "has elegido "+valorItem, Toast.LENGTH_SHORT).show();
     }
 
     @Override
